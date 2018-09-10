db39af127f45a1ae7e4658e4687cc722c9b3c3cb
pi03k <pi03k87@gmail.com>
2018-09-10 12:31:12 +0200

    File truncating

--- ex16_file_read_write.py
+++ ex16_file_read_write.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+from sys import argv
+
+script, filename = argv
+
+
+def eraseFile(filename):
+    print("Erasing file")
+    file = open(filename, 'w')
+    file.truncate()
+    file.close()
+
+
+print(f"We are going to erase {filename}")
+answer = input('[Y/n] ')
+
+if answer == 'Y':
+    eraseFile(filename)
