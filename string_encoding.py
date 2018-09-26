25e8c91ae1f93666ee8d51324d08b8681f0b0429
pi03k <pi03k87@gmail.com>
2018-09-26 18:15:41 +0200

    strings encoding

---
+++ string_encoding.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def main(language_file, encoding, errors):
+    line = language_file.readline()
+    if line:
+        print_line(line, encoding, errors)
+        return main(language_file, encoding, errors)
+
+
+def print_line(line, encoding, errors):
+    nextLang = line.strip()
+    rawBytes = nextLang.encode(encoding, errors=errors)
+    cookedString = rawBytes.decode(encoding, errors=errors)
+    print(rawBytes, "<==>", cookedString)
+
+
+if __name__ == "__main__":
+    script, inputFile, inputEncoding, error = sys.argv
+    languages = open(inputFile, encoding='utf-8')
+    main(languages, inputEncoding, error)
